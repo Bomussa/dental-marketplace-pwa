462ecb14228af84b993e80ced7658bb73a747421
Iyad Mousa <bomussa@hotmail.com>
2026-09-08 10:02:32 +0300

    chore: remove unused legacy server readiness helper

--- lib/server-readiness.ts
+++
@@ -1,6 +0,0 @@
-export function serverOperationsConfigured() {
-  return Boolean(
-    process.env.NEXT_PUBLIC_SUPABASE_URL &&
-      (process.env.SUPABASE_SECRET_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY),
-  );
-}
